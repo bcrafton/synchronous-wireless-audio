a3b5c8fd80cbd7269f06c7b64954a10b8c092c7b
brian crafton <brian crafton>
2016-12-17 02:45:43 -0500

    moving files to Desktop, not sig commit

--- server/setup.py
+++ server/setup.py
@@ -1,8 +1,9 @@
-
+from socket import *
+import pyaudio  
+import wave 
 from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser, NmapParserException
-
-
+import os
 
 def get_ip_list():
 	ip_list = []
@@ -28,4 +29,27 @@ def get_ip_list():
 	return ip_list
 			
 ip_list = get_ip_list()
-print ip_list
+ 
+num_bytes = 32000
+file_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir, "goat.wav"))
+f = wave.open(file_path,"rb") 
+
+data = f.readframes(num_bytes / f.getnchannels() / f.getsampwidth())
+
+counter = 0
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+
+serverSocket.listen(1)
+connectionSocket, addr = serverSocket.accept()
+
+while data != '':
+	data = f.readframes(num_bytes / f.getnchannels() / f.getsampwidth())
+	connectionSocket.send(data)
+	
+	counter = counter + 1
+	print counter
+
+connectionSocket.close()
