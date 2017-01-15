40734a075b008253c94b2b919ecac937a8460b8a
Brian Crafton <crafton.b@husky.neu.edu>
2017-01-15 15:43:45 -0500

    adding template code for server

---
+++ src/server/server.c
@@ -0,0 +1,24 @@
+
+static char* ip_address_list = NULL;
+static char* filepath = NULL;
+
+// just making this 100 because itll never be that big
+// if someone wants to be this list resizeable then go for it
+static int sockfd[100];
+static int num_sockets;
+
+server_status_code_t start()
+{
+    return SUCCESS:
+}
+
+server_status_code_t set_song(char* filepath)
+{
+    return SUCCESS;
+}
+
+server_status_code_t set_devices(char* ip_address_list, char delimmeter)
+{   
+    return SUCCESS:
+}
+
---
+++ src/server/server.h
@@ -0,0 +1,19 @@
+
+typedef enum server_status_code_t server_status_code_t
+
+enum server_status_code_t{
+    SUCCESS = 0,
+    ERROR
+};
+
+// this initializes the server c code.
+// this will start a thread in an infinate while loop and check for status changes and play songs.
+server_status_code_t start();
+
+// this will set change the song to be played.
+server_status_code_t set_song(char* filepath);
+
+// this will change the set of devices.
+// if devices are not included from the argument list then kill the tcp connections
+// if devices are included from the argument list then leave the connectins open
+server_status_code_t set_devices(char* ip_address_list, char delimmeter);
