16bd68161cbbabbad881bc4b7d45f749b483d360
Brian Crafton <crafton.b@husky.neu.edu>
2017-01-19 01:21:29 +0000

    commitng server example file

--- examples/c++/tcp/server.cpp
+++ examples/c++/tcp/server.cpp
@@ -1,19 +1,3 @@
-/* A simple server in the internet domain using TCP.
-myServer.c
-D. Thiebaut
-Adapted from http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
-The port number used in 51717.
-This code is compiled and run on the Raspberry as follows:
-   
-    g++ -o myServer myServer.c
-    ./myServer
-
-The server waits for a connection request from a client.
-The server assumes the client will send positive integers, which it sends back multiplied by 2.
-If the server receives -1 it closes the socket with the client.
-If the server receives -2, it exits.
-*/
-
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -24,47 +8,26 @@ If the server receives -2, it exits.
 #include <unistd.h>
 #include <errno.h>
 
+// this will be used to hardcode how big the audio packets will be
+#define SIZE_OF_FRAME 	1000
 
 void error( char *msg ) {
   perror(  msg );
   exit(1);
 }
 
-int func( int a ) {
-   return 2 * a;
-}
-
-void sendData( int sockfd, int x ) {
-  int n;
-
-  char buffer[32];
-  sprintf( buffer, "%d\n", x );
-  if ( (n = write( sockfd, buffer, strlen(buffer) ) ) < 0 )
-    error( const_cast<char *>( "ERROR writing to socket") );
-  buffer[n] = '\0';
-}
-
-int getData( int sockfd ) {
-  char buffer[32];
-  int n;
-
-  if ( (n = read(sockfd,buffer,31) ) < 0 )
+void getData( int sockfd, void* buffer, unsigned int size ) {
+  int status = read(sockfd, buffer, size);
+  if (status < 0)
+  {
     error( const_cast<char *>( "ERROR reading from socket") );
-  buffer[n] = '\0';
-  return atoi( buffer );
+  }
 }
 
 int main(int argc, char *argv[]) {
      int sockfd, newsockfd, portno = 51200, clilen;
-     char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     int data;
 
-     int x;
-     printf( "using port #%d\n", portno );
-     printf("%d\n", portno);
-     
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0)
          error( const_cast<char *>("ERROR opening socket") );
@@ -79,30 +42,19 @@ int main(int argc, char *argv[]) {
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
  
-     //--- infinite wait on a connection ---
      while ( 1 ) {
-        printf( "waiting for new client...\n" );
         if ( ( newsockfd = accept( sockfd, (struct sockaddr *) &cli_addr, (socklen_t*) &clilen) ) < 0 )
             error( const_cast<char *>("ERROR on accept") );
-        printf( "opened new communication with client\n" );
-        while ( 1 ) {
-             //---- wait for a number from client ---
-             data = getData( newsockfd );
-             printf( "got %d\n", data );
-             if ( data < 0 )
-                break;
-               
-             data = func( data );
 
-             //--- send new data back ---
-             printf( "sending back %d\n", data );
-             sendData( newsockfd, data );
+        int* buffer = (int*) malloc(sizeof(int) * 100);
+        getData(newsockfd, buffer, sizeof(int) * 100);
+        int i;
+        for(i=0; i<100; i++)
+        {
+          printf("%d\n", buffer[i]);
         }
+        
         close( newsockfd );
-
-        //--- if -2 sent by client, we can quit ---
-        if ( data == -2 )
-          break;
      }
      return 0;
 }
