d9240ad7a6e36c8f6a233aeb8186e67780e8d4ac
Brian Crafton <crafton.b@husky.neu.edu>
2017-01-19 19:42:01 -0500

    pushing example updates and server update

--- examples/c++/tcp/client.cpp
+++ examples/c++/tcp/client.cpp
@@ -11,56 +11,102 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
+#define PORTNO 51200
+
 void error(char *msg) {
     perror(msg);
     exit(0);
 }
 
-void sendData( int sockfd, void* buffer, unsigned int size ) 
-{
+typedef struct sockaddr_in sockaddr_in;
+typedef struct hostent     hostent;
 
-  int status = write( sockfd, buffer, size);
-  if (status < 0)
-  {
-    error( const_cast<char *>( "ERROR writing to socket") );
-  }
-  else
-  {
-    printf("Great Success!\n");
-  }
+typedef struct device{
+    int sockfd;
+    hostent *server;
+    sockaddr_in serv_addr;
+} device_t;
+
+static device_t* devices;
+static int num_devices;
+
+void send_data(void* buffer, unsigned int size) 
+{
+    int i;
+    for(i=0; i<num_devices; i++)
+    {
+        int status = write( devices[i].sockfd, buffer, size);
+        if (status < 0)
+        {
+            error( const_cast<char *>( "ERROR writing to socket") );
+        }
+        else
+        {
+            printf("Great Success!\n");
+        }
+    }
+}
+
+void setup(char* ip_addresses, char delimeter, int num)
+{
+    num_devices = num;
+
+    char** ip_address_list = (char**) malloc(sizeof(char*) * num_devices);
+
+    char* p = strtok(ip_addresses, &delimeter);
+    
+    int i;
+    for(i=0; i<num_devices; i++)
+    {
+        ip_address_list[i] = strdup(p);
+        p = strtok(NULL, &delimeter);
+    }
+
+    devices = (device_t*) malloc(sizeof(device_t) * num_devices);
+   
+    for(i=0; i<num_devices; i++)
+    {
+        if ( ( devices[i].sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 )
+        {
+            error( const_cast<char *>( "ERROR opening socket") );
+        }
+        if ( ( devices[i].server = gethostbyname( ip_address_list[i] ) ) == NULL )
+        {
+            error( const_cast<char *>("ERROR, no such host\n") );
+        }
+
+        bzero( (char *) &devices[i].serv_addr, sizeof(devices[i].serv_addr));
+        devices[i].serv_addr.sin_family = AF_INET;
+        bcopy( (char *)devices[i].server->h_addr, (char *)&devices[i].serv_addr.sin_addr.s_addr, devices[i].server->h_length);
+        devices[i].serv_addr.sin_port = htons(PORTNO);
+
+        if ( connect(devices[i].sockfd,(struct sockaddr *)&devices[i].serv_addr,sizeof(devices[i].serv_addr)) < 0)
+        {
+            error( const_cast<char *>( "ERROR connecting") );
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno = 51200, n;
-    char serverIp[] = "192.168.0.100";
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    if ( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 )
-        error( const_cast<char *>( "ERROR opening socket") );
-
-    if ( ( server = gethostbyname( serverIp ) ) == NULL )
-        error( const_cast<char *>("ERROR, no such host\n") );
-   
-    bzero( (char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy( (char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        error( const_cast<char *>( "ERROR connecting") );
+    char ip_addresses[] = ";192.168.0.100;192.168.0.102";
+    char delimeter = ';';
+    setup(ip_addresses, delimeter, 2);
 
     int* buffer = (int*) malloc(sizeof(int) * 100);
     int i;
     for(i=1; i<100; i++)
     {
-      buffer[i] = i;
+        buffer[i] = i;
     }
 
-    sendData(sockfd, buffer, sizeof(int) * 100);
+    send_data(buffer, sizeof(int) * 100);
 
     while(1);
 
-    close( sockfd );
+    for(i=0; i<num_devices; i++)
+    {
+        close( devices[i].sockfd );
+    }
     return 0;
 }
