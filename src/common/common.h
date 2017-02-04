7adeec375c9d420dabaf6325af8a5a9c44cd2e0c
Brian Crafton <crafton.b@husky.neu.edu>
2017-02-03 19:16:08 -0500

    pi does not like this

--- src/common/common.h
+++ src/common/common.h
@@ -28,7 +28,7 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long uint64_t;
+//typedef unsigned long uint64_t;
 
 /*
 typedef char int8_t;
