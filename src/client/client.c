ea29ac5f9a163f5a8e8d2c43efaf354027b1dade
Kevin Wilson <kwilson94@Kevins-MacBook-Pro.local>
2017-02-09 12:15:48 -0500

    uncommented some spec settings

--- src/client/client.c
+++ src/client/client.c
@@ -28,9 +28,9 @@ int main(int argc, char *argv[]) {
     // allocate the tcp swap buffer
     swap_buf = (uint8_t*) malloc(sizeof(uint8_t) * FRAME_SIZE);
 
-    //SDL_memset(&spec, 0, sizeof(spec));
-    //spec.freq = 48000;
-    //spec.channels = 2;
+    SDL_memset(&spec, 0, sizeof(spec));
+    spec.freq = 48000;
+    spec.channels = 2;
     //spec.samples = 4096;
     spec.callback = callback;
     spec.userdata = NULL;
