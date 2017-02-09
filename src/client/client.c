30d35293db3e56b9a324be00e9e1fee3e3f53e12
Kevin Wilson <kwilson94@gmail.com>
2017-02-09 14:48:46 -0500

    change assertion for frame_size divisibility

--- src/client/client.c
+++ src/client/client.c
@@ -122,9 +122,8 @@ void wait_for_connection()
 
 void callback(void *userdata, Uint8 *stream, int len) {
     // printf("callback len: %d\n", len);
-	assert(len == FRAME_SIZE);
-    // len = ( len > FRAME_SIZE ? FRAME_SIZE : len );
-    // len = 4096;
+    assert(len % FRAME_SIZE == 0);
+    int frames = len / FRAME_SIZE;
 
 	uint8_t* data = read_buffer(rbuf, FRAME_SIZE);
 	if (data == NULL)
