aeab7c7281c7df91af864941f5161e9dc764198a
Brian Crafton <crafton.b@husky.neu.edu>
2017-03-28 04:29:54 +0000

    switching to 32 bit

--- src/client/client.c
+++ src/client/client.c
@@ -154,7 +154,7 @@ static void* run_tcp_thread(void *data)
                 struct timespec t;
                 clock_gettime(CLOCK_REALTIME, &t);
                 uint64_t time = t.tv_nsec;
-                printf("%lu %lu %d\n", time, control_data.time, control_data.packet_number);
+                printf("%d %d %d %d\n", t.tv_sec, control_data.sec, t.tv_nsec, control_data.nsec);
                 
 #if(!LOCAL_HOST_ONLY)
                 SDL_CloseAudio();
